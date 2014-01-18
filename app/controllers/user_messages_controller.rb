6a67641dbb318a89cab182378f9f01401def1b38
Jay Greasley <jay@fliz.co.uk>
2014-01-18 15:41:21 +0000

    pass create message test

--- app/controllers/user_messages_controller.rb
+++ app/controllers/user_messages_controller.rb
@@ -2,11 +2,15 @@ class UserMessagesController < ApplicationController
   
   def create
     
-    m = Message.new
-    m.save
+    Message.create message_params
     redirect_to root_url # use root url because http spec says redirect has to include full url. use root_path in views
     
   end
   
+  def message_params
+    
+    params[:message].permit :body
+    
+  end
   
 end
--- spec/requests/user_messages_spec.rb
+++ spec/requests/user_messages_spec.rb
@@ -5,8 +5,8 @@ describe "UserMessages" do
   describe "POST /user_messages" do
     it "creates a message" do
       
-      post user_messages_path
-      Message.count.should == 1
+      post user_messages_path, message: { body: 'Test' }
+      Message.first.body.should eq 'Test'
       
     end
   end
