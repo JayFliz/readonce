ae269a01ff9db17aa20e5ec27f3764429f68a4c3
Jay Greasley <jay@fliz.co.uk>
2014-01-18 19:54:36 +0000

    weirdness

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -16,13 +16,11 @@ describe "Messages" do
     
     it "redirects to /messages" do
       
-<<<<<<< HEAD:spec/requests/user_messages_spec.rb
+
       response.status.should eq 302 # could remove this as the second test implies a redirect
       response.location.should eq user_messages_url
-=======
       response.status.should eq 302
       response.location.should eq messages_url
->>>>>>> user-creates-a-message:spec/requests/messages_spec.rb
       
       
     end
@@ -34,11 +32,8 @@ describe "Messages" do
     it "redirect to the messages index" do
       # capybara methods
       visit new_message_path 
-<<<<<<< HEAD:spec/requests/user_messages_spec.rb
       fill_in :message_body, with: 'Test' #brittle, but hey, watcha gonna do??
-=======
       fill_in :message_body, with: 'Test'
->>>>>>> user-creates-a-message:spec/requests/messages_spec.rb
       click_button 'Send'
       page.should have_content 'Test'
       
